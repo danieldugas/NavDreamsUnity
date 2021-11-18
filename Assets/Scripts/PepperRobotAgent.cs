47427aa79f88eaac73a3e1825f7741bcfce6f7be
daniel <exodaniel@gmail.com>
2021-11-18 16:28:19 +0100

    fix joint definitions

--- Assets/Scripts/PepperRobotAgent.cs
+++ Assets/Scripts/PepperRobotAgent.cs
@@ -8,18 +8,39 @@ using Unity.MLAgents.Actuators;
 /// Simulated Pepper for RL training,
 public class PepperRobotAgent : Agent
 {
-    Rigidbody rBody;
+    
     public Transform Target;
+
+    // Effectors (actions): Joint angles, force applied to base rigid body
+    public HingeJoint HeadYawParent;
+    public HingeJoint HeadPitchParent;
+    public HingeJoint LShoulderPitchParent;
+    public HingeJoint LShoulderRollParent;
+    public HingeJoint LElbowYawParent;
+    public HingeJoint LElbowRollParent;
+    public HingeJoint LWristYawParent;
+    public HingeJoint LHandParent;
+    public HingeJoint RShoulderPitchParent;
+    public HingeJoint RShoulderRollParent;
+    public HingeJoint RElbowYawParent;
+    public HingeJoint RElbowRollParent;
+    public HingeJoint RWristYawParent;
+    public HingeJoint RHandParent;
+    public HingeJoint HipRollParent;
+    public HingeJoint HipPitchParent;
+    public HingeJoint KneePitchParent;
+    public Rigidbody BaseRBody;
+    // Sensors (observations)
+    public Camera FrontColorCamera;
     // Start is called before the first frame update
     void Start()
     {
-        rBody = GetComponent<Rigidbody>();
     }
 
     public override void OnEpisodeBegin()
     {
-        this.rBody.angularVelocity = Vector3.zero;
-        this.rBody.velocity = Vector3.zero;
+        this.BaseRBody.angularVelocity = Vector3.zero;
+        this.BaseRBody.velocity = Vector3.zero;
         this.transform.localPosition = new Vector3( 0, 0.5f, 0);
     }
 
@@ -30,7 +51,7 @@ public class PepperRobotAgent : Agent
         sensor.AddObservation(this.transform.localPosition);
 
         // Agent velocity
-        sensor.AddObservation(rBody.velocity.x);
-        sensor.AddObservation(rBody.velocity.z);
+        sensor.AddObservation(BaseRBody.velocity.x);
+        sensor.AddObservation(BaseRBody.velocity.z);
     }
 }

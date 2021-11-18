4a524cf4e5d329bcdb387ad43f2714436588ed37
daniel <exodaniel@gmail.com>
2021-11-18 21:27:19 +0100

    realistic looking scene, fixes

--- Assets/Scripts/PepperRobotAgent.cs
+++ Assets/Scripts/PepperRobotAgent.cs
@@ -9,6 +9,7 @@ using Unity.MLAgents.Actuators;
 public class PepperRobotAgent : Agent
 {
     
+    public float forceMultiplier = 10;
     public Transform Target;
 
     // Effectors (actions): Joint angles, force applied to base rigid body
@@ -32,6 +33,9 @@ public class PepperRobotAgent : Agent
     public Rigidbody BaseRBody;
     // Sensors (observations)
     public Camera FrontColorCamera;
+    public int image_width = 64;
+    public int image_height = 64;
+    private CameraSensor cameraSensor;
     // Start is called before the first frame update
     void Start()
     {
@@ -54,4 +58,44 @@ public class PepperRobotAgent : Agent
         sensor.AddObservation(BaseRBody.velocity.x);
         sensor.AddObservation(BaseRBody.velocity.z);
     }
+
+
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+        // Actions, size = 2
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = actionBuffers.ContinuousActions[0];
+        controlSignal.z = actionBuffers.ContinuousActions[1];
+        Vector3 required_vel_adjustment = controlSignal - BaseRBody.transform.InverseTransformDirection(BaseRBody.velocity); // local frame
+        Vector3 required_accel = required_vel_adjustment;
+        Vector3 required_force = required_accel * forceMultiplier;
+        Vector3 required_force_wf = BaseRBody.transform.TransformDirection(required_force);
+        required_force_wf.y = 0.0f; // we can't affect vertical acceleration
+        if (forceMultiplier != 0.0f) {
+        BaseRBody.AddForce(required_force_wf);
+        BaseRBody.AddTorque(-BaseRBody.transform.right * required_force.z * 0.2f);
+        } // NOCOMMIT
+        // Rewards
+        float distanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
+
+        // Reached target
+        if (distanceToTarget < 1.42f)
+        {
+            SetReward(1.0f);
+            EndEpisode();
+        }
+
+        // Fell off platform
+        else if (this.transform.localPosition.y < 0 || this.transform.localPosition.y > 5)
+        {
+            EndEpisode();
+        }
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+{
+    var continuousActionsOut = actionsOut.ContinuousActions;
+    continuousActionsOut[0] = Input.GetAxis("Horizontal");
+    continuousActionsOut[1] = Input.GetAxis("Vertical");
+}
 }
